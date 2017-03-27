658d3d734cde6236242bdbf8ae8ca8ad8e3fef7a
海神 <watatsumi@haishendeMac-mini.local>
2017-03-27 17:33:21 +0800

    创建文件

---
+++ 本地管理头像昵称工具类/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  本地管理头像昵称工具类
+//
+//  Created by 海神 on 2017/3/27.
+//  Copyright © 2017年 海神. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 本地管理头像昵称工具类/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  本地管理头像昵称工具类
+//
+//  Created by 海神 on 2017/3/27.
+//  Copyright © 2017年 海神. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
